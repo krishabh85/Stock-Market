9df184fb8eac1bb69a76f700b9c8d1fe10bf9c5c
Rishabh Khandelwal <krishabh85@gmail.com>
2020-09-06 12:57:58 +0530

    Admin Server added

---
+++ admin-server/admin-server/src/test/java/com/socg/adminserver/AdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socg.adminserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
